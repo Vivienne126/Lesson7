e4552e9176d2e69cd8b434aea981135cf2cd86dd
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-31 17:21:51 +0530

    New

---
+++ Activity1.py
@@ -0,0 +1,14 @@
+#Identity operator
+x=5
+if (type(x) is int):
+    print("True")
+else:
+    print("False")
+
+#Membership Operator
+y="Hello"
+z="Welcome"
+if (y in z):
+    print("True")
+else:
+    print("False")
